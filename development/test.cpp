020a1411fa08d5661bf50c7e2730b72037fa1cfb
0x53616D62696B <patrik.maraczek@thermofisher.com>
2024-11-12 13:06:35 +0100

    renaming test to development

--- development/test.cpp
+++ development/test.cpp
@@ -1,13 +1,13 @@
 #include "../src/Utils/Logger/Logger.hpp"
+
 #include <chrono>
 #include <format>
 #include <iostream>
 #include <ctime>
 
-int main(int argc, char* argv[]){
-    if (argc!= 2){
-    }
 
+
+void time_development_tests(){
     Log(LogLevel::Info, "Logging from main thread");
     printf("Starting\n\n");
     std::chrono::time_point hello = std::chrono::system_clock::now();
@@ -21,6 +21,15 @@ int main(int argc, char* argv[]){
     char buf[100];
     std::strftime(buf, sizeof buf, "%D %T", std::gmtime(&ts.tv_sec));
     std::cout << "Current time: " << buf << '.' << ts.tv_nsec << " UTC\n";
+}
+
+
+int main(int argc, char* argv[]){
+    if (argc!= 2){
+    }
+
+    //time_development_tests();
+
 
     return 0;
 }
