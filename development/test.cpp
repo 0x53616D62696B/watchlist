33500b0d276b7258cea645bf055e1d8141d4d118
0x53616D62696B <patrikmaraczek@gmail.com>
2024-11-15 18:50:47 +0100

    compiler version added

--- development/test.cpp
+++ development/test.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <ctime>
 
+#include "test.hpp"
+
 
 
 void time_development_tests(){
@@ -23,13 +25,44 @@ void time_development_tests(){
     std::cout << "Current time: " << buf << '.' << ts.tv_nsec << " UTC\n";
 }
 
+void print_compiler_version(){
+
+    if (_MSC_VER){ //msvc
+        std::cout << "MSVC Compiler ";
+        if (_MSC_VER >= 1920) std::cout << "C++20";
+        else if (_MSC_VER >= 1910) std::cout << "C++17";
+        else if (_MSC_VER >= 1900) std::cout << "C++14";
+        else if (_MSC_VER >= 1800) std::cout << "C++11";
+        else if (_MSC_VER >= 1500) std::cout << "C++98";
+        else std::cout << "pre-standard C++.";
+        std::cout << "\n";
+    }
+    else { //gcc
+        std::cout << "GCC Compiler ";
+        if (__cplusplus == 202101L) std::cout << "C++23";
+        else if (__cplusplus == 202002L) std::cout << "C++20";
+        else if (__cplusplus == 201703L) std::cout << "C++17";
+        else if (__cplusplus == 201402L) std::cout << "C++14";
+        else if (__cplusplus == 201103L) std::cout << "C++11";
+        else if (__cplusplus == 199711L) std::cout << "C++98";
+        else std::cout << "pre-standard C++." << __cplusplus;
+        std::cout << "\n";
+    }   
+}
 
 int main(int argc, char* argv[]){
     if (argc!= 2){
     }
 
+    print_compiler_version();
+
     //time_development_tests();
 
+    std::string ip_address = "172.28.208.1";
+    char* ip_address_char = new char[ip_address.length() + 1]; // Initialize the ip_address_char variable
+    // std::strncpy(reinterpret_cast<char*>(ip_address_char), ip_address.c_str(), ip_address.length());
+    strncpy_s(ip_address_char, ip_address.length() + 1, ip_address.c_str(), ip_address.length());
+    //delete[] ip_address_char; // Deallocate the memory
 
     return 0;
 }
---
+++ development/test.hpp
@@ -0,0 +1,1 @@
+void print_compiler_version();
