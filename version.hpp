8f5ac87094bfb7b94ff73d024a8743911b1b085f
0x53616D62696B <patrikmaraczek@gmail.com>
2025-10-09 11:05:04 +0200

    version.hpp

--- version.hpp
+++ version.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
-#define VERSION_MAJOR 
-#define VERSION_MINOR 
-#define VERSION_PATCH 
-#define VERSION_STRING ".."
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 0
+#define VERSION_PATCH 1
+#define VERSION_STRING "0.0.1"
 #define VERSION_PRERELEASE ""
 #define VERSION_BUILD ""
-#define VERSION_BRANCH "unknown"
-#define VERSION_COMMIT "unknown"
-#define VERSION_FULL "..+unknown.unknown"
+#define VERSION_BRANCH "feature-versioning"
+#define VERSION_COMMIT "95ac3b9"
+#define VERSION_FULL "0.0.1+feature-versioning.95ac3b9"
