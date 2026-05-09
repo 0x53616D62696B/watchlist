6bbacd26a7394f671f0e2b1b8f6ecab30e40e989
0x53616D62696B <patrikmaraczek@gmail.com>
2026-05-09 21:54:13 +0200

    Version.hpp ignored. It is generated when building from GitVersion. No need to track it.

---
+++ src/Common/VersionExample.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 0
+#define VERSION_PATCH 1
+#define VERSION_STRING "0.0.1"
+#define VERSION_PRERELEASE ""
+#define VERSION_BUILD ""
+#define VERSION_BRANCH ""
+#define VERSION_COMMIT "d52ab3a"
+#define VERSION_FULL "0.0.1"
